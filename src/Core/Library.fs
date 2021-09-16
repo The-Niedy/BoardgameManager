b08e685f2dcad5c6fc3d273ec1ab1e05c553810d
NicoVIII <nico@nicoviii.net>
2021-09-16 17:11:07 +0000

    Use dotnet 6.0 for units of measures for uint

--- src/Core/Library.fs
+++ src/Core/Library.fs
@@ -1,5 +1,15 @@
 ﻿namespace BoardgameManager.Core
 
+[<Measure>]
+type cm
+
+type Measures = {
+    height: uint<cm>
+    width1: uint<cm>
+    width2: uint<cm>
+}
+
 type Boardgame = {
     name: string
+    measures: Measures
 }
